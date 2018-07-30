05cbbe3fdd687bc3b06eb6fb8d23f81d66262b94
Tianshu Xu <tiansx15@vt.edu>
2018-07-30 20:10:53 +0000

    added launch files

--- smartsense_ws/src/smartsense_launch/launch/lidar.sh
+++ smartsense_ws/src/smartsense_launch/launch/lidar.sh
@@ -1,1 +1,1 @@
-rosrun urg_node urg_node _ip_address:="192.168.0.10" | rviz -d lidar.rviz | rosbag record -a
+rosrun urg_node urg_node _ip_address:="192.168.0.10" | rviz -d lidar.rviz | rosbag record -a -o /media/nvidia/07bc9976-1c58-42ee-aa74-ce46823142d8/
--- smartsense_ws/src/smartsense_launch/launch/zed_run.sh
+++ smartsense_ws/src/smartsense_launch/launch/zed_run.sh
@@ -1,1 +1,2 @@
-roslaunch zed_wrapper zed.launch | rosbag record -a
+roslaunch zed_wrapper zed.launch | rosbag record /zed/left/camera_info_raw /zed/right/camera_info_raw /zed/left/image_raw_color/compressed /zed/right/image_raw_color/compressed /zed/odom /zed/point_cloud/cloud_registered /tf -o /media/nvidia/07bc9976-1c58-42ee-aa74-ce46823142d8/
+
